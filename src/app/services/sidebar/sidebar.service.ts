997dc1d943e6a5d8ad2a4e85885d56d491cf88c2
jmaureirac <jmaureira@icci.cl>
2018-05-31 03:02:52 -0400

    servicio de sidebar cargando items

--- src/app/services/sidebar/sidebar.service.ts
+++ src/app/services/sidebar/sidebar.service.ts
@@ -3,6 +3,43 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class SidebarService {
 
+  menu: any[] = [
+    {
+      titulo: 'Principal',
+      icono: 'mdi mdi-gauge',
+      submenu: [
+        { titulo: 'Dashboard', url: 'dashboard' },
+        { titulo: 'login', url: '/login' },        
+        { titulo: 'registro', url: '/register' }
+      ]
+    },
+    {
+      titulo: 'Productos',
+      icono: 'mdi mdi-lightbulb-on-outline',
+      submenu: [
+        { titulo: 'Productos', url: 'productos' },
+        { titulo: 'Inventario', url: 'inventario' }
+      ]
+    },
+    {
+      titulo: 'Cotizaciones',
+      icono: 'mdi mdi-content-paste',
+      submenu: [
+        { titulo: 'Cotizar', url: 'cotizar' },
+        { titulo: 'Cotizaciones', url: 'cotizaciones' }
+      ]
+    },
+    {
+      titulo: 'Clasificación',
+      icono: 'mdi mdi-information-outline',
+      submenu: [
+        { titulo: 'Categorias', url: 'clasificacion/categorias' },
+        { titulo: 'Subcategorias', url: 'clasificacion/subcategorias' },
+        { titulo: 'Marcas', url: 'clasificacion/marcas' },
+      ]
+    }
+  ];
+
   constructor() { }
 
 }
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SidebarService } from '../../services/service.index';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
@@ -7,7 +10,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public _sideBarService: SidebarService,
+    public router: Router
+  ) { }
 
   ngOnInit() {
   }
