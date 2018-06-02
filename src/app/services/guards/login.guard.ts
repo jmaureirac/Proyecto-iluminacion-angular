0e82f59615c547f96e37b53a59a8257efdfe98db
jmaureirac <jmaureira@icci.cl>
2018-06-02 01:15:45 -0400

    login guard inicializado y integrado

---
+++ src/app/services/guards/login.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { UserService } from '../user/user.service';
+
+@Injectable()
+export class LoginGuard implements CanActivate {
+
+  constructor(
+    private _userService: UserService,
+    private route: Router
+  ) {
+
+  }
+
+  canActivate() {
+    
+    console.log(this._userService.token);
+
+    if ( this._userService.isLogged() ) {
+      return true;
+    } else {
+      this.route.navigate(['/login']);
+      return false;
+    }
+  }
+}
