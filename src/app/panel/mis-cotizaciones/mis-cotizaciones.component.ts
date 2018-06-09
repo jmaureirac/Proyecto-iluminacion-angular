259e53b4e386b74c92bfa3789641029091855690
jmaureirac <jmaureira@icci.cl>
2018-06-09 04:27:54 -0400

    componentes inicializados

---
+++ src/app/panel/mis-cotizaciones/mis-cotizaciones.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mis-cotizaciones',
+  templateUrl: './mis-cotizaciones.component.html',
+  styles: []
+})
+export class MisCotizacionesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
