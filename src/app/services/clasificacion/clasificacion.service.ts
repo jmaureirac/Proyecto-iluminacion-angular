50a251483a78032fa13cf1bcde82b69718025010
jmaureirac <jmaureira@icci.cl>
2018-06-17 01:05:39 -0400

    servicios para agregar marca/categoria/subcategoria

--- src/app/services/clasificacion/clasificacion.service.ts
+++ src/app/services/clasificacion/clasificacion.service.ts
@@ -2,13 +2,19 @@ import { Injectable } from '@angular/core';
 import { URL_SERVER } from '../../config/config';
 import { UserService } from '../user/user.service';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { Marca } from '../../models/marca.model';
+import { Categoria } from '../../models/categoria.model';
+import { Subcategoria } from '../../models/subcategoria.model';
 
 @Injectable()
 export class ClasificacionService {
 
   constructor(
     public _userService: UserService,
-    public http: HttpClient
+    public http: HttpClient,
+    public router: Router
   ) { }
 
   // Marcas
@@ -18,6 +24,39 @@ export class ClasificacionService {
     return this.http.get( url )
       .map( (res: any) => {
         return res.marcas;
+      })
+      .catch( err => {
+        if ( err.status === 401 ) {
+          swal('¡Cuidado!', 'Ha expirado su sesión, vuelva a ingresar', 'info');
+          this._userService.logout();       
+          this.router.navigate(['/login']);
+          return Observable.throw(err);
+        } else { 
+          swal('¡Error!', 'Error al obtener datos', 'error');
+          return Observable.throw(err);
+        }
+      });
+      
+  }
+
+  setMarca( marca: Marca ) {
+    let url = URL_SERVER + '/marca?token=' + this._userService.token;
+
+    return this.http.post( url, marca )
+      .map( (res: any) => {
+        swal('¡Correcto!', 'Marca agregada correctamente', 'success');
+        return res;
+      })
+      .catch( err => {
+        if ( err.status === 401 ) {
+          swal('¡Cuidado!', 'Ha expirado su sesión, vuelva a ingresar', 'info');
+          this._userService.logout();       
+          this.router.navigate(['/login']);
+          return Observable.throw(err);
+        } else { 
+          swal('¡Error!', 'Error al ingresar datos', 'error');
+          return Observable.throw(err);
+        }
       });
       
   }
@@ -30,6 +69,17 @@ export class ClasificacionService {
     return this.http.get( url )
       .map( (res: any) => {
         return res.categorias;
+      })
+      .catch( err => {
+        if ( err.status === 401 ) {
+          swal('¡Cuidado!', 'Ha expirado su sesión, vuelva a ingresar', 'info');
+          this._userService.logout();       
+          this.router.navigate(['/login']);
+          return Observable.throw(err);
+        } else { 
+          swal('¡Error!', 'Error al obtener datos', 'error');
+          return Observable.throw(err);
+        }
       });
   }
 
@@ -39,8 +89,42 @@ export class ClasificacionService {
     return this.http.get( url )
       .map( (res: any) => {
         return res.categoria;
+      })
+      .catch( err => {
+        if ( err.status === 401 ) {
+          swal('¡Cuidado!', 'Ha expirado su sesión, vuelva a ingresar', 'info');
+          this._userService.logout();       
+          this.router.navigate(['/login']);
+          return Observable.throw(err);
+        } else { 
+          swal('¡Error!', 'Error al obtener datos', 'error');
+          return Observable.throw(err);
+        }
       });
   }
+
+  setCategoria( categoria: Categoria ) {
+    let url = URL_SERVER + '/categoria?token=' + this._userService.token;
+
+    return this.http.post( url, categoria )
+      .map( (res: any) => {
+        swal('¡Correcto!', 'Categoria agregada correctamente', 'success');
+        return res;
+      })
+      .catch( err => {
+        if ( err.status === 401 ) {
+          swal('¡Cuidado!', 'Ha expirado su sesión, vuelva a ingresar', 'info');
+          this._userService.logout();       
+          this.router.navigate(['/login']);
+          return Observable.throw(err);
+        } else { 
+          swal('¡Error!', 'Error al ingresar datos', 'error');
+          return Observable.throw(err);
+        }
+      });
+      
+  }
+
 
   // Subcategorias
   getSubcategorias() {
@@ -49,6 +133,17 @@ export class ClasificacionService {
     return this.http.get( url )
       .map( (res: any) => {
         return res.subcategorias;
+      })
+      .catch( err => {
+        if ( err.status === 401 ) {
+          swal('¡Cuidado!', 'Ha expirado su sesión, vuelva a ingresar', 'info');
+          this._userService.logout();       
+          this.router.navigate(['/login']);
+          return Observable.throw(err);
+        } else { 
+          swal('¡Error!', 'Error al obtener datos', 'error');
+          return Observable.throw(err);
+        }
       });
   }
 
@@ -58,9 +153,40 @@ export class ClasificacionService {
     return this.http.get( url )
       .map( (res: any) => {
         return res.subcategoria;
+      })
+      .catch( err => {
+        if ( err.status === 401 ) {
+          swal('¡Cuidado!', 'Ha expirado su sesión, vuelva a ingresar', 'info');
+          this._userService.logout();       
+          this.router.navigate(['/login']);
+          return Observable.throw(err);
+        } else { 
+          swal('¡Error!', 'Error al obtener datos', 'error');
+          return Observable.throw(err);
+        }
       });
   }
 
+  setSubcategoria( subcategoria: Subcategoria ) {
+    let url = URL_SERVER + '/subcategoria?token=' + this._userService.token;
 
+    return this.http.post( url, subcategoria )
+      .map( (res: any) => {
+        swal('¡Correcto!', 'Subcategoria agregada correctamente', 'success');
+        return res;
+      })
+      .catch( err => {
+        if ( err.status === 401 ) {
+          swal('¡Cuidado!', 'Ha expirado su sesión, vuelva a ingresar', 'info');
+          this._userService.logout();       
+          this.router.navigate(['/login']);
+          return Observable.throw(err);
+        } else { 
+          swal('¡Error!', 'Error al ingresar datos', 'error');
+          return Observable.throw(err);
+        }
+      });
+      
+  }
 
 }
