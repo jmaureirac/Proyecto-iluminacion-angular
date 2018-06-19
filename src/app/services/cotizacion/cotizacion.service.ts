0d843a8e0d4e5ec73993e21fc35f32b8744ba0d7
jmaureirac <jmaureira@icci.cl>
2018-06-19 00:44:39 -0400

    service: cotizacion service cargado con servicios rest

--- src/app/services/cotizacion/cotizacion.service.ts
+++ src/app/services/cotizacion/cotizacion.service.ts
@@ -1,8 +1,103 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { UserService } from '../user/user.service';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { URL_SERVER } from '../../config/config';
+import { Cotizacion } from '../../models/cotizacion.model';
 
 @Injectable()
 export class CotizacionService {
 
-  constructor() { }
+  constructor(
+    public _userService: UserService,
+    public http: HttpClient,
+    public router: Router
+  ) { }
+
+  getCotizaciones() {
+    let url = URL_SERVER + '/cotizacion?token=' + this._userService.token;
+
+    return this.http.get(url)
+      .map( (res: any) => {
+        return res;
+      })
+      .catch( err => {
+        if ( err.status === 401 ) {
+          swal('¡Cuidado!', 'Ha expirado su sesión, vuelva a ingresar', 'info');
+          this._userService.logout();
+          this.router.navigate(['/login']);       
+          return Observable.throw(err);
+        } else { 
+          swal('¡Error!', 'Error al actualizar datos', 'error');
+          return Observable.throw(err);
+        }
+      });
+      
+  }
+
+  getCotizacionById( cotizacion: Cotizacion ) {
+    let url = URL_SERVER + '/cotizacion/' + cotizacion._id + '?token=' + this._userService.token;
+
+    return this.http.get(url)
+      .map( (res: any) => {
+        return res;
+      })
+      .catch( err => {
+        if ( err.status === 401 ) {
+          swal('¡Cuidado!', 'Ha expirado su sesión, vuelva a ingresar', 'info');
+          this._userService.logout();
+          this.router.navigate(['/login']);       
+          return Observable.throw(err);
+        } else { 
+          swal('¡Error!', 'Error al actualizar datos', 'error');
+          return Observable.throw(err);
+        }
+      });
+      
+  }
+
+  getCotizacionesUser( id: string ) {
+    let url = URL_SERVER + '/cotizacion/user/' + id + '?token=' + this._userService.token;
+
+    return this.http.get(url)
+      .map( (res: any) => {
+        return res;
+      })
+      .catch( err => {
+        if ( err.status === 401 ) {
+          swal('¡Cuidado!', 'Ha expirado su sesión, vuelva a ingresar', 'info');
+          this._userService.logout();
+          this.router.navigate(['/login']);       
+          return Observable.throw(err);
+        } else { 
+          swal('¡Error!', 'Error al actualizar datos', 'error');
+          return Observable.throw(err);
+        }
+      });
+      
+  }
+
+  setCotizacion( cotizacion: Cotizacion ) {
+    let url = URL_SERVER + '/cotizacion?token=' + this._userService.token;
+
+    return this.http.post(url, cotizacion)
+      .map( (res: any) => {
+        swal('¡Correcto!', 'Cotizacion creada correctamente', 'success');
+        return true;
+      })
+      .catch( err => {
+        if ( err.status === 401 ) {
+          swal('¡Cuidado!', 'Ha expirado su sesión, vuelva a ingresar', 'info');
+          this._userService.logout();
+          this.router.navigate(['/login']);       
+          return Observable.throw(err);
+        } else { 
+          swal('¡Error!', 'Error al actualizar datos', 'error');
+          return Observable.throw(err);
+        }
+      });
+      
+  }
 
 }
