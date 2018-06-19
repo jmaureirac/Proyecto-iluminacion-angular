0d1590312eadb7aa1f7209844a342d84d2c591ad
jmaureirac <jmaureira@icci.cl>
2018-06-19 00:46:11 -0400

    ver todas las cotizaciones funcional y con modal para ver cotizacion

--- src/app/panel/cotizacion/cotizacion.component.ts
+++ src/app/panel/cotizacion/cotizacion.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService, CotizacionService, ViewCotizacionService } from '../../services/service.index';
+import { Cotizacion } from '../../models/cotizacion.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cotizacion',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CotizacionComponent implements OnInit {
 
-  constructor() { }
+  cotizaciones: Cotizacion[] = [];
+
+  constructor(
+    public _userService: UserService,
+    public _cotizacionService: CotizacionService,
+    public _modalCotizacion: ViewCotizacionService,
+    public router: Router
+  ) { }
 
   ngOnInit() {
+    this.cargarCotizaciones();
+  }
+
+  cargarCotizaciones() {
+    this._cotizacionService.getCotizaciones()
+      .subscribe( res => this.cotizaciones = res.cotizaciones ); 
   }
 
 }
