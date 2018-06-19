f7889529c5e7066202daa675fbc163d257b6f9c0
jmaureirac <jmaureira@icci.cl>
2018-06-19 00:45:50 -0400

    mis cotizaciones funcional y modal ver cotizacion

--- src/app/panel/mis-cotizaciones/mis-cotizaciones.component.ts
+++ src/app/panel/mis-cotizaciones/mis-cotizaciones.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CotizacionService, UserService, ViewCotizacionService } from '../../services/service.index';
+import { Cotizacion } from '../../models/cotizacion.model';
 
 @Component({
   selector: 'app-mis-cotizaciones',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MisCotizacionesComponent implements OnInit {
 
-  constructor() { }
+  misCotizaciones: Cotizacion[] = [];
+
+  constructor(
+    public _cotizacionService: CotizacionService,
+    public _userService: UserService,
+    public _modalCotizacion: ViewCotizacionService
+  ) { }
 
   ngOnInit() {
+    this.cargarMisCotizaciones();
   }
 
+  cargarMisCotizaciones() {
+    this._cotizacionService.getCotizacionesUser( this._userService.user._id )
+      .subscribe( res => this.misCotizaciones = res.cotizaciones );
+  }
 }
