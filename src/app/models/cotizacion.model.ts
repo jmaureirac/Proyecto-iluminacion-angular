577a046f069cc4164fd9b79bcbdad9fe76565b6f
jmaureirac <jmaureira@icci.cl>
2018-06-19 00:40:19 -0400

    model: cotizacion model cargado

---
+++ src/app/models/cotizacion.model.ts
@@ -0,0 +1,12 @@
+export class Cotizacion {
+
+    constructor(
+        public user: any,
+        public productos: any[],
+        public created_at?: string,
+        public _id?: string
+    ) {
+
+    }
+
+}
