2c77d2e38036e86210897e94a8b7de4f19450262
jmaureirac <jmaureira@icci.cl>
2018-06-20 20:25:24 -0400

    service: agregado servicio de obtener producto por id

--- src/app/services/producto/producto.service.ts
+++ src/app/services/producto/producto.service.ts
@@ -5,6 +5,7 @@ import { UserService } from '../user/user.service';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../../models/producto.model';
 import { Observable } from 'rxjs/Observable';
+import { Location } from '@angular/common';
 
 
 @Injectable()
@@ -13,7 +14,8 @@ export class ProductoService {
   constructor(
     public _userService: UserService,
     public http: HttpClient,
-    public router: Router
+    public router: Router,
+    public location: Location
   ) { }
 
   getProductosPaginados( desde: number = 0 ) {
@@ -88,6 +90,48 @@ export class ProductoService {
           return Observable.throw(err);
         } else { 
           swal('¡Error!', 'Error al eliminar producto', 'error');
+          return Observable.throw(err);
+        }
+      });
+      
+  }
+
+  updateProducto( producto: Producto ) {
+    let url = URL_SERVER + '/producto/' + producto._id + '?token=' + this._userService.token;
+
+    return this.http.put( url, producto )
+      .map( (res: any) => {
+        swal('¡Correcto!', 'Producto actualizado correctamente', 'success');
+        return true;
+      })
+      .catch( err => {
+        if ( err.status === 401 ) {
+          swal('¡Cuidado!', 'Ha expirado su sesión, vuelva a ingresar', 'info');
+          this._userService.logout();
+          this.router.navigate(['/login']);       
+          return Observable.throw(err);
+        } else { 
+          swal('¡Error!', 'Error al actualizar datos', 'error');
+          return Observable.throw(err);
+        }
+      });
+      
+  }
+
+  getProductoByID( id: string ) {
+    let url = URL_SERVER + '/producto/' + id + '?token=' + this._userService.token;
+
+    return this.http.get( url )
+      .map( (res: any) => {
+        return res;
+      })
+      .catch( err => {
+        if ( err.status === 401 ) {
+          swal('¡Cuidado!', 'Ha expirado su sesión, vuelva a ingresar', 'info');
+          this.router.navigate(['/login']);       
+          return Observable.throw(err);
+        } else { 
+          this.location.back();
           return Observable.throw(err);
         }
       });
