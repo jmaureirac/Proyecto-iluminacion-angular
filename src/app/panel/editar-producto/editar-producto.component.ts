68cdd60edfca92c7c8ec47f1ad655fb562d869e3
jmaureirac <jmaureira@icci.cl>
2018-07-02 00:35:28 -0400

    editar producto terminado mas carga de imagen

--- src/app/panel/editar-producto/editar-producto.component.ts
+++ src/app/panel/editar-producto/editar-producto.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductoService, ClasificacionService } from '../../services/service.index';
 import { Router, ActivatedRoute } from '@angular/router';
-import { Producto } from '../../models/producto.model';
 import { Location } from '@angular/common';
 import { Marca } from '../../models/marca.model';
 import { Subcategoria } from '../../models/subcategoria.model';
+import { NgForm } from '@angular/forms';
 
+declare function init_dropify();
 
 @Component({
   selector: 'app-editar-producto',
@@ -14,11 +15,17 @@ import { Subcategoria } from '../../models/subcategoria.model';
 })
 export class EditarProductoComponent implements OnInit {
 
-  producto: Producto;
+  producto: any;
   id: string;
+  categoria: string; 
+
+  current_sub: string;
+  current_marca: string;
 
   marcas: Marca[] = [];
   subcategorias: Subcategoria[] = [];
+
+  imagen: File;
 
   constructor(
     public _productoService: ProductoService,
@@ -41,13 +48,27 @@ export class EditarProductoComponent implements OnInit {
         this.location.back();
       } else {
         this.buscarProducto( params['id'] );
+        init_dropify();
       }
     } ) ;
   }
 
   buscarProducto( id: string ) {
     this._productoService.getProductoByID( id )
-      .subscribe( res => this.producto = res.producto );
+      .subscribe( res => {
+        this.producto = res.producto;
+        this.current_marca = res.producto.marca.nombre;
+        this.current_sub = res.producto.subcategoria.nombre;
+        this.cargarCategoria( this.producto.subcategoria.categoria );
+        if ( !this.producto.medidas ) {
+          Object.assign(this.producto, { medidas: { altura: 0, largo: 0, ancho: 0 } });
+        }
+      });
+  }
+
+  async cargarCategoria( id: string ) {
+    await this._clasificacionService.getCategoriasById( id )
+      .subscribe( res => this.categoria = res.nombre );
   }
 
   async cargarClasificacion() {
@@ -55,4 +76,39 @@ export class EditarProductoComponent implements OnInit {
     await this._clasificacionService.getSubcategorias().subscribe( subcategorias => this.subcategorias = subcategorias );
   }
 
+  guardarCambios() {
+    if ( typeof this.producto.subcategoria === 'object' ) {
+      this.producto.subcategoria = this.producto.subcategoria._id;
+    }
+    if ( typeof this.producto.marca === 'object' ) {
+      this.producto.marca = this.producto.marca._id;
+    }
+
+    this._productoService.updateProducto( this.producto ).subscribe(this.router.navigate['/panel/productos']);
+    
+  }
+
+  onChange( id_subcategoria: string ) {
+    this._clasificacionService.getSubcategoriaById(id_subcategoria)
+      .subscribe( res => this.categoria = res.categoria.nombre);
+
+  }
+
+  recargar() {
+    location.reload();
+  }
+
+  seleccion( archivo: File ) {
+    this.imagen = archivo;
+  }
+
+  subirImagen() {
+    this._productoService.updateImageProduct( this.imagen, this.producto._id )
+      .then( res => {
+        this.buscarProducto( this.producto._id );
+      })
+      .catch( err => {
+        console.log('Error en la carga de imagen');
+      }) ;
+  }
 }
