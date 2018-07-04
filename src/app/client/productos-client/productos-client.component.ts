0ab32efe31ff83eb73c7faa6e646ba6edfc52b53
jmaureirac <jmaureira@icci.cl>
2018-07-04 01:44:25 -0400

    productos obtenidos y filtrados por categorias para renderizar separados en cards

--- src/app/client/productos-client/productos-client.component.ts
+++ src/app/client/productos-client/productos-client.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductoService } from '../../services/service.index';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-productos-client',
@@ -7,9 +9,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductosClientComponent implements OnInit {
 
-  constructor() { }
+  prueba: number[] = [1, 2, 3];
+
+  total: number;
+  productos: any[] = [];
+  subcategorias: string[] = [];
+
+  productosPorSubcategoria: any[] = [];
+
+  constructor(
+    public _productoService: ProductoService,
+    public router: Router
+  ) { }
 
   ngOnInit() {
+    this.cargarProductos();
+    
+  }
+
+  async cargarProductos() {
+    await this._productoService.getAllProductos()
+      .subscribe( res => {
+        this.total = res.total;
+        this.productos = res.productos;
+        this.obtenerSubcategorias( this.productos );
+      });
+  }
+
+  obtenerSubcategorias( productos: any[] ) {
+    productos.forEach(producto => {
+      if ( !this.subcategorias.includes( producto.subcategoria.nombre ) ) {
+        // se le quita espacio al nombre, porque se bindea como id de elemento html y no lee elementos con ' ' en el id
+        this.subcategorias.push( producto.subcategoria.nombre.split(' ').join('_') ); 
+      }
+    });
+    this.separarProductosSubcategoria( productos, this.subcategorias );
+  }
+
+  separarProductosSubcategoria( productos: any[], subcategorias: string[] ) {
+    subcategorias.forEach( nombre => {
+      this.productosPorSubcategoria.push({
+        id: nombre,
+        subcategoria: nombre.split('_').join(' '),
+        productos: this.filtrarProductos( nombre )
+      });
+    });
+    console.log(this.productosPorSubcategoria);
+
+  }
+
+  filtrarProductos( subcategoria: string ) {
+    let arrayProductos = [];
+    this.productos.forEach( prod => {
+      if ( prod.subcategoria.nombre === subcategoria.split('_').join(' ') ) {
+        arrayProductos.push( prod );
+      }
+    });
+    return arrayProductos;
   }
 
 }
